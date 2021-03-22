5d63bbb836e135bd091beb269bf636ceb2bb7cbb
sincerity <791588195@qq.com>
2021-03-22 18:26:36 +0800

    fix(plugin): 增加PluginImpl实现接口校验

--- 2021-03-06/multi_modules_plugin/src/main/kotlin/com/github/skiiyis/plugin/MultiModulesByteCodeCollectWeaver.kt
+++ 2021-03-06/multi_modules_plugin/src/main/kotlin/com/github/skiiyis/plugin/MultiModulesByteCodeCollectWeaver.kt
@@ -9,7 +9,7 @@ import org.objectweb.asm.Opcodes
 class MultiModulesByteCodeCollectWeaver : BaseWeaver() {
 
     override fun isWeavableClass(fullQualifiedClassName: String): Boolean {
-        return fullQualifiedClassName.startsWith("com.skiiyis") &&
+        return fullQualifiedClassName.startsWith(CUSTOM_PACKAGE_NAME_PRE) &&
                 super.isWeavableClass(fullQualifiedClassName)
     }
 
@@ -21,6 +21,7 @@ class MultiModulesByteCodeCollectWeaver : BaseWeaver() {
         ClassVisitor(Opcodes.ASM5, classWriter) {
 
         private lateinit var `implement`: String
+        private var interfaces: Array<out String>? = null
 
         override fun visit(
             version: Int,
@@ -31,13 +32,14 @@ class MultiModulesByteCodeCollectWeaver : BaseWeaver() {
             interfaces: Array<out String>?
         ) {
             `implement` = name
+            this.interfaces = interfaces
             super.visit(version, access, name, signature, superName, interfaces)
         }
 
         override fun visitAnnotation(desc: String, visible: Boolean): AnnotationVisitor {
             val vs = super.visitAnnotation(desc, visible)
-            return if (desc == "Lcom/skiiyis/center/PluginImpl;")
-                MultiModulesByteCodeAnnotationVisitor(`implement`, vs)
+            return if (desc == PLUGIN_IMPL_CLASS_DESC)
+                MultiModulesByteCodeAnnotationVisitor(interfaces, `implement`, vs)
             else
                 vs
         }
@@ -45,6 +47,7 @@ class MultiModulesByteCodeCollectWeaver : BaseWeaver() {
 
     // Class
     class MultiModulesByteCodeAnnotationVisitor(
+        private val interfaces: Array<out String>?,
         private val `implement`: String,
         av: AnnotationVisitor
     ) :
@@ -52,8 +55,30 @@ class MultiModulesByteCodeCollectWeaver : BaseWeaver() {
 
         // -> 筛选PluginImpl的 value 内容
         override fun visit(name: String?, value: Any?) {
-            MultiModulesAnnotationMap.cache[value.toString()] = `implement`
+            val valueDesc = value.toString()
+            interfaces?.forEach {
+                if (isHitTargetClass(it, valueDesc)) {
+                    MultiModulesAnnotationMap.cache[valueDesc] = `implement`
+                }
+            }
             super.visit(name, value)
         }
+
+        /**
+         * 命中目标class
+         * @param interface com/skiiyis/center/Plugin
+         * @param valueDesc Lcom/skiiyis/center/Plugin;
+         */
+        private fun isHitTargetClass(`interface`: String, valueDesc: String): Boolean {
+            if (`interface`.isEmpty() || valueDesc.isEmpty()) {
+                return false
+            }
+            return `interface`.substring(1, `interface`.length - 1) == valueDesc
+        }
+    }
+
+    companion object {
+        private const val PLUGIN_IMPL_CLASS_DESC = "Lcom/skiiyis/center/PluginImpl;"
+        private const val CUSTOM_PACKAGE_NAME_PRE = "com.skiiyis"
     }
 }
